a1a77280833452541fecb6c53d654ab06d9560c8
Ruben Diaz <rdiaz-fr@student.42malaga.com>
2024-03-04 13:24:05 +0100

    Update libraries and main function

--- inc/so_long.h
+++ inc/so_long.h
@@ -1,23 +1,44 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "minilibx-linux/mlx.h"
+// Main libraries
+# include "mlx/include/MLX42/MLX42.h"
+# include <fcntl.h>
 # include <stdbool.h>
-# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-# PX_SIZE 32
-# MALLOC_ERROR 1
+// Error definitions
+# define MALLOC_ERROR 1
 
-# WALL '1'
-# PLAYER 'P'
-# EXIT 'E'
-# PATH '0'
+// Map
+# define WALL '1'
+# define PLAYER 'P'
+# define EXIT 'E'
+# define EMPTY '0'
+# define COLLECTIBLE 'C'
 
-typedef struct	s_game {
-	void		*mlx_con;
-	void		*mlx_win;
-	t_data		*data;
-	t_texture	*texture;
-}		t_game;
+// Window size
+# define WIDTH 800
+# define HEIGHT 600
+
+// Window title
+# define TITLE "Guatemala"
+
+// Movement
+# define UP 'W'
+# define LEFT 'A'
+# define DOWN 'S'
+# define RIGHT 'D'
+
+typedef struct s_data
+{
+	mlx_t	*mlx;
+	int		width;
+	int		height;
+	int		move_counter;
+	int		collected;
+	char	**map;
+}			t_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdiaz-fr <rdiaz-fr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 11:45:17 by rdiaz-fr          #+#    #+#             */
+/*   Updated: 2024/03/04 13:20:47 by rdiaz-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/so_long.h"
 
-int main(int ac, char **av)
+/**
+ * @brief Main function, here we will execute all the other
+ * functions. This is the most important function.
+ *
+ * @param ac
+ * @param av
+ * @return int
+ */
+int	main(void)
 {
-	t_game	*win;
-	t_game	*connection;
+	t_data	data;
 
-	connection = mlx_init();
-	win = mlx_new_window();
-
-	mlx_loop(connection);
+	data.mlx_ptr = mlx_init(WIDTH, HEIGHT, TITLE, false);
+	if (!data.mlx_ptr)
+		return (MALLOC_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WIDTH, HEIGHT, TITLE);
+	if (!data.win_ptr)
+		return (free(data.mlx_ptr), 1);
+	// Loop
+	mlx_loop(data.mlx_ptr);
+	return (0);
 }
